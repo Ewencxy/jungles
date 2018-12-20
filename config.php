a13a0043c6f1887d2e8a364df1d39dbf509e7973
Ewen Chan <ewencxy@gmail.com>
2018-12-20 11:07:57 -0800

    added stuff

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+error_reporting(E_ALL & ~E_NOTICE);
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'c9');
+define('DB_CHARSET', 'utf8');
+define('DB_USER', 'alexlien');
+define('DB_PASSWORD', '');
+?>
